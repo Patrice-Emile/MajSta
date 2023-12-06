503f9cfa97ab89a0268954711f8bb20c76eabec6
Patrice-Emile <cpatop@outlook.fr>
2023-12-06 13:40:53 +0100

    init package

---
+++ programs/src/importConfig.php
@@ -0,0 +1,12 @@
+<?php
+require 'vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+class Config
+{
+    const COLUMNS_TO_DELETE = array(
+        'colonne1',
+        'colonne2'
+    );
+}
